b142d1917a4907cea213458d61e7be72db825e2a
Santana Woollery <santanawoollery@gmail.com>
2021-08-09 16:00:49 -0400

    HTTPS

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
-​
+
 const router = express.Router();
-​
+
 /* GET users listing. */
 router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
     User.find()
@@ -15,7 +15,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req
         })
         .catch(err => next(err));
 });
-​
+
 router.post('/signup', (req, res) => {
     User.register(
         new User({ username: req.body.username }),
@@ -49,14 +49,14 @@ router.post('/signup', (req, res) => {
         }
     );
 });
-​
+
 router.post('/login', passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({ _id: req.user._id });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
-​
+
 router.get('/logout', (req, res, next) => {
     if (req.session) {
         req.session.destroy();
@@ -68,5 +68,5 @@ router.get('/logout', (req, res, next) => {
         return next(err);
     }
 });
-​
+
 module.exports = router;
