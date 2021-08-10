4d5e4867c7f3c5ee2b052c0061937b01c846d57c
Santana Woollery <santanawoollery@gmail.com>
2021-08-10 16:03:03 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '995808591193602',
+        clientSecret: '95734549fe1e5747fd44e1c7550a3222'
+    }
 }
